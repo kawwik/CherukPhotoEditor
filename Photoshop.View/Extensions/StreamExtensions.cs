4f5c95c04fa753169722488b7a7bf37bda5e779a
Mikhail <misha.rukovishnikow@yandex.ru>
2022-10-06 00:29:53 +0300

    Причесал контекст

---
+++ Photoshop.View/Extensions/StreamExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Photoshop.View.Extensions;
+
+public static class StreamExtensions
+{
+    public static byte[] ReadToEnd(this Stream stream)
+    {
+        var bytes = new byte[stream.Length];
+        int offset = 0;
+        while (stream.Position != stream.Length - 1)
+        {
+            offset += stream.Read(bytes, offset, (int)stream.Length);
+        }
+
+        return bytes;
+    }
+}
--- Photoshop.View/ViewModels/PhotoEditionContext.cs
+++ Photoshop.View/ViewModels/PhotoEditionContext.cs
@@ -4,13 +4,12 @@ using Photoshop.Domain.ImageEditors.Factory;
 using Photoshop.Domain.Images.Factory;
 using Photoshop.View.Commands;
 using Photoshop.View.Converters;
+using Photoshop.View.Extensions;
 using ReactiveUI;
 
 namespace Photoshop.View.ViewModels;
 public class PhotoEditionContext : ReactiveObject
 {
-    private readonly IImageFactory _imageFactory;
-    private readonly IImageEditorFactory _imageEditorFactory;
     private readonly IImageConverter _imageConverter;
     
     private IImageEditor _imageEditor;
@@ -23,8 +22,6 @@ public class PhotoEditionContext : ReactiveObject
         IImageConverter imageConverter, 
         IImageEditor imageEditor)
     {
-        _imageFactory = imageFactory;
-        _imageEditorFactory = imageEditorFactory;
         _imageConverter = imageConverter;
         _imageEditor = imageEditor;
         
@@ -32,23 +29,22 @@ public class PhotoEditionContext : ReactiveObject
         OpenImage = openImage;
         OpenImage.StreamCallback = stream =>
         {
-            var bytes = new byte[]{};
-            stream.Read(bytes, 0, (int)stream.Length);
-            var image = _imageFactory.GetImage(bytes);
+            var bytes = stream.ReadToEnd();
+            var image = imageFactory.GetImage(bytes);
 
             var imageData = image.GetData();
-            ImageEditor = _imageEditorFactory.GetImageEditor(imageData);
+            ImageEditor = imageEditorFactory.GetImageEditor(imageData);
         };
     }
 
     public OpenImageCommand OpenImage { get; }
     public SaveImageCommand SaveImage { get; }
 
-    public IImageEditor ImageEditor
+    public IImage Image => _imageConverter.ConvertToBitmap(ImageEditor.GetData());
+
+    private IImageEditor ImageEditor
     {
         get => _imageEditor;
         set => _imageEditor = this.RaiseAndSetIfChanged(ref _imageEditor, value);
     }
-
-    public IImage Image => _imageConverter.ConvertToBitmap(ImageEditor.GetData());
 }
