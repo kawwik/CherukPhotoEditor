6afe80988aeb5bf62ac4e0a8c4e7fdfe5f8beb93
Cherkovetc Denis Olegovich <cherkovetsd@niuitmo.ru>
2022-11-24 18:45:38 +0300

    Добавил функционал гамма-коррекции в ImageEditor и выбор стартовой гаммы в ImageEditorFactory

--- Photoshop.Domain/ImageEditors/GrayImageEdtior.cs
+++ Photoshop.Domain/ImageEditors/GrayImageEdtior.cs
@@ -2,19 +2,36 @@
 
 public class GrayImageEditor : IImageEditor
 {
-    private readonly ImageData _imageData;
-    
-    public GrayImageEditor(ImageData imageData)
+    private ImageData _imageData;
+    private float _imageGamma;
+    private float _outputGamma;
+    private readonly IGammaConverter _gammaConverter;
+    public GrayImageEditor(ImageData imageData, float imageGamma, float outputGamma, IGammaConverter gammaConverter)
     {
         if (imageData.PixelFormat is not PixelFormat.Gray)
             throw new Exception("Картинка должна быть серой");
 
         _imageData = imageData;
+        _imageGamma = imageGamma;
+        _outputGamma = outputGamma;
+        _gammaConverter = gammaConverter;
     }
     
     public ImageData GetData() => _imageData;
-    public ImageData GetRgbData(bool[]? channels) => _imageData;
+    
+    public ImageData GetRgbData(bool[]? channels = default) =>
+        _gammaConverter.ConvertGamma(_imageData, _imageGamma, _outputGamma);
 
+    public void ConvertGamma(float gamma)
+    {
+        _imageData = _gammaConverter.ConvertGamma(_imageData, _imageGamma, gamma);
+        _imageGamma = gamma;
+    }
+
+    public void AssignGamma(float gamma)
+    {
+        _outputGamma = gamma;
+    }
     public void SetColorSpace(ColorSpace newColorSpace)
     {
         throw new NotSupportedException($"Изменение цветового пространства не поддерживается в {GetType().Name}");
--- Photoshop.Domain/ImageEditors/RgbImageEditor.cs
+++ Photoshop.Domain/ImageEditors/RgbImageEditor.cs
@@ -4,22 +4,39 @@ public class RgbImageEditor : IImageEditor
 {
     private ImageData _imageData;
     private ColorSpace _colorSpace;
+    private float _imageGamma = 1;
+    private float _outputGamma = 1;
     private readonly IColorSpaceConverter _colorSpaceConverter;
+    private readonly IGammaConverter _gammaConverter;
 
-    public RgbImageEditor(ImageData imageData, ColorSpace colorSpace, IColorSpaceConverter colorSpaceConverter)
+    public RgbImageEditor(ImageData imageData, ColorSpace colorSpace, float imageGamma, float outputGamma, IColorSpaceConverter colorSpaceConverter, IGammaConverter gammaConverter)
     {
         if (imageData.PixelFormat is not PixelFormat.Rgb)
             throw new Exception("Картинка должна быть в формате RGB");
 
         _imageData = imageData;
         _colorSpace = colorSpace;
+        _imageGamma = imageGamma;
+        _outputGamma = outputGamma;
         _colorSpaceConverter = colorSpaceConverter;
+        _gammaConverter = gammaConverter;
     }
 
     public ImageData GetData() => _imageData;
 
     public ImageData GetRgbData(bool[]? channels = default) =>
-        _colorSpaceConverter.ToRgb(_imageData, _colorSpace, channels);
+        _gammaConverter.ConvertGamma(_colorSpaceConverter.ToRgb(_imageData, _colorSpace, channels), _imageGamma, _outputGamma);
+
+    public void ConvertGamma(float gamma)
+    {
+        _imageData = _gammaConverter.ConvertGamma(_imageData, _imageGamma, gamma);
+        _imageGamma = gamma;
+    }
+
+    public void AssignGamma(float gamma)
+    {
+        _outputGamma = gamma;
+    }
 
     public void SetColorSpace(ColorSpace newColorSpace)
     {
