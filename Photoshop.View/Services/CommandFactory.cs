75b747f4a0da2c0000bc47ff66718c44687529b8
Cherkovetc Denis Olegovich <cherkovetsd@niuitmo.ru>
2022-12-02 02:51:43 +0300

    Добавил генератор градиентов

--- Photoshop.View/Services/CommandFactory.cs
+++ Photoshop.View/Services/CommandFactory.cs
@@ -3,6 +3,7 @@ using System.Reactive;
 using System.Threading.Tasks;
 using Photoshop.Domain;
 using Photoshop.Domain.ImageEditors;
+using Photoshop.Domain.ImageEditors.Factory;
 using Photoshop.View.Services.Interfaces;
 using ReactiveUI;
 
@@ -12,11 +13,13 @@ public class CommandFactory : ICommandFactory
 {
     private readonly IImageService _imageService;
     private readonly IDialogService _dialogService;
+    private readonly IImageEditorFactory _imageEditorFactory;
 
-    public CommandFactory(IImageService imageService, IDialogService dialogService)
+    public CommandFactory(IImageService imageService, IDialogService dialogService, IImageEditorFactory imageEditorFactory)
     {
         _imageService = imageService;
         _dialogService = dialogService;
+        _imageEditorFactory = imageEditorFactory;
     }
     
     public ReactiveCommand<ColorSpace, IImageEditor?> OpenImage() =>
@@ -50,6 +53,26 @@ public class CommandFactory : ICommandFactory
 
     private IImageEditor GenerateGradientInternal()
     {
-        throw new NotImplementedException();
+        int width = 100;
+        int height = 100;
+        PixelFormat pixelFormat = PixelFormat.Gray;
+        
+        int coef = pixelFormat == PixelFormat.Rgb ? 3 : 1;
+        float[] newPixels = new float[height * width * coef];
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                int pos = i * width + j;
+                for (int col = 0; col < coef; col++)
+                {
+                    int ind = pos * coef + col;
+                    newPixels[ind] = 255 * (1.0f * j / width);
+                }
+            }
+        }
+        
+        return _imageEditorFactory.GetImageEditor(new ImageData(newPixels, pixelFormat, height, width), ColorSpace.Rgb, 1);
     }
 }
