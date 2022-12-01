d3b64fda0b6091221398056711c2bcbb0324c43f
Cherkovetc Denis Olegovich <cherkovetsd@niuitmo.ru>
2022-12-01 23:16:15 +0300

    Добавил поддержку дизеринга в ImageEditor и ImageEditorFactory

--- Photoshop.Domain/ImageEditors/Factory/ImageEditorFactory.cs
+++ Photoshop.Domain/ImageEditors/Factory/ImageEditorFactory.cs
@@ -4,19 +4,21 @@ public class ImageEditorFactory : IImageEditorFactory
 {
     private readonly IColorSpaceConverter _colorSpaceConverter;
     private readonly IGammaConverter _gammaConverter;
+    private readonly IDitheringConverter _ditheringConverter;
 
-    public ImageEditorFactory(IColorSpaceConverter colorSpaceConverter, IGammaConverter gammaConverter)
+    public ImageEditorFactory(IColorSpaceConverter colorSpaceConverter, IGammaConverter gammaConverter, IDitheringConverter ditheringConverter)
     {
         _colorSpaceConverter = colorSpaceConverter;
         _gammaConverter = gammaConverter;
+        _ditheringConverter = ditheringConverter;
     }
 
-    public IImageEditor GetImageEditor(ImageData imageData, ColorSpace colorSpace, float imageGamma)
+    public IImageEditor GetImageEditor(ImageData imageData, ColorSpace colorSpace, float imageGamma, DitheringType ditheringType, int ditheringDepth)
     {
         return imageData.PixelFormat switch
         {
-            PixelFormat.Rgb => new RgbImageEditor(imageData, colorSpace, imageGamma, _colorSpaceConverter, _gammaConverter),
-            PixelFormat.Gray => new GrayImageEditor(imageData, imageGamma, _gammaConverter),
+            PixelFormat.Rgb => new RgbImageEditor(imageData, colorSpace, imageGamma, ditheringType, ditheringDepth, _colorSpaceConverter, _gammaConverter, _ditheringConverter),
+            PixelFormat.Gray => new GrayImageEditor(imageData, imageGamma, ditheringType, ditheringDepth, _gammaConverter, _ditheringConverter),
             _ => throw new NotSupportedException($"Данный формат картинки не поддерживается: {imageData.PixelFormat}")
         };
     }
--- Photoshop.Domain/ImageEditors/GrayImageEdtior.cs
+++ Photoshop.Domain/ImageEditors/GrayImageEdtior.cs
@@ -4,21 +4,34 @@ public class GrayImageEditor : IImageEditor
 {
     private ImageData _imageData;
     private float _imageGamma;
+    private DitheringType _ditheringType;
+    private int _ditheringDepth;
     private readonly IGammaConverter _gammaConverter;
-    public GrayImageEditor(ImageData imageData, float imageGamma, IGammaConverter gammaConverter)
+    private readonly IDitheringConverter _ditheringConverter;
+    
+    public GrayImageEditor(ImageData imageData, float imageGamma, DitheringType ditheringType, int ditheringDepth, IGammaConverter gammaConverter, IDitheringConverter ditheringConverter)
     {
         if (imageData.PixelFormat is not PixelFormat.Gray)
             throw new Exception("Картинка должна быть серой");
 
+        if (ditheringDepth < 1 || ditheringDepth > 8)
+            throw new ArgumentException("Некорректная глубина дизеринга");
+        
         _imageData = imageData;
         _imageGamma = imageGamma;
+        _ditheringType = ditheringType;
+        _ditheringDepth = ditheringDepth;
         _gammaConverter = gammaConverter;
+        _ditheringConverter = ditheringConverter;
     }
     
     public ImageData GetData() => _imageData;
     
     public ImageData GetRgbData(float gamma, bool[]? channels = default) =>
-        _gammaConverter.ConvertGamma(_imageData, _imageGamma, gamma);
+        _ditheringConverter.Convert(_gammaConverter.ConvertGamma(_imageData, _imageGamma, gamma), _ditheringType, _ditheringDepth);
+
+    public ImageData GetSaveData() =>
+        _ditheringConverter.Convert(_imageData, _ditheringType, _ditheringDepth);
 
     public void ConvertGamma(float gamma)
     {
@@ -29,5 +42,20 @@ public class GrayImageEditor : IImageEditor
     public void SetColorSpace(ColorSpace newColorSpace)
     {
         throw new NotSupportedException($"Изменение цветового пространства не поддерживается в {GetType().Name}");
+    }
+    
+    public void SetDitheringType(DitheringType newType)
+    {
+        _ditheringType = newType;
+    }
+
+    public void SetDitheringDepth(int newDepth)
+    {
+        if (newDepth < 1 || newDepth > 8)
+        {
+            throw new ArgumentException("Некорректная глубина дизеринга");
+        }
+
+        _ditheringDepth = newDepth;
     }
 }
